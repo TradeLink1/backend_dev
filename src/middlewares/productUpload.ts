c9f2a50e42e7447dab92bf8d28a96b96565c4cfc
okoro <mi.fracri91@gmail.com>
2025-09-01 23:58:28 +0100

    added test img

--- src/middlewares/productUpload.ts
+++ src/middlewares/productUpload.ts
@@ -1,31 +1,16 @@
-// import multer from "multer";
-
-// // Configure storage for products
-// const productStorage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     // Set the destination folder for product images
-//     cb(null, "public/uploads/products");
-//   },
-//   filename: (req, file, cb) => {
-//     // Create a unique filename for the product image
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(
-//       null,
-//       file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
-//     );
-//   },
-// });
-
-// // Create a new multer instance for products
-// export const productUpload = multer({ storage: productStorage });
-
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: "./uploads/products/",
+const productStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads/products");
+  },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
   },
 });
 
-export const productUpload = multer({ storage });
+export const productUpload = multer({ storage: productStorage });
